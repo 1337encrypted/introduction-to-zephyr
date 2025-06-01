0e7e6bf2ee39275ea25ac2d1686f0fb5d0c6cf1d
1337encrypted <smallaszero@gmail.com>
2025-06-01 23:48:29 +0530

    Modified 04_button and added support for button1, button2 and leds

--- workspace/apps/04_button/src/main.c
+++ workspace/apps/04_button/src/main.c
@@ -4,31 +4,43 @@
 
 // Settings
 static const int32_t sleep_ms = 100;
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(DT_ALIAS(push_button), gpios);
+static const struct gpio_dt_spec button1 = GPIO_DT_SPEC_GET(DT_ALIAS(push_button1), gpios);
+static const struct gpio_dt_spec button2 = GPIO_DT_SPEC_GET(DT_ALIAS(push_button2), gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(yellow_led), gpios);
 
 int main(void) {
-    int ret, state;
+    int ret, state1, state2;
     
-    if(!gpio_is_ready_dt(&button)) {
+    if(!gpio_is_ready_dt(&button1) || !gpio_is_ready_dt(&button2)) {
         printk("ERROR: Button device not ready\r\n");
         return -1;
     }
 
-    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if(!gpio_is_ready_dt(&led)) {
+        printk("ERROR: LED device not ready\r\n");
+        return -1;
+    }
+
+    ret = gpio_pin_configure_dt(&button1, GPIO_INPUT);
+    ret |= gpio_pin_configure_dt(&button2, GPIO_INPUT);
+    ret |= gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+
     if(ret < 0) {
-        printk("ERROR: Failed to configure button pin %d\r\n", button.pin);
+        printk("ERROR: Failed to configure GPIO pins\r\n");
         return ret;
     }
 
-    printk("Button spec flags: 0x%x\r\n", button.dt_flags);
+    printk("GPIO pins configured successfully\r\n");
 
     while(1) {
-        state = gpio_pin_get_dt(&button);
-        if(state < 0) {
+        state1 = gpio_pin_get_dt(&button1);
+        state2 = gpio_pin_get_dt(&button2);
+
+        if(state1 < 0 || state2 < 0) {
             printk("ERROR: Failed to read button state\r\n");
-            return state;
+            return state1 | state2;
         } else {
-            printk("Button state: %d\r\n", state);
+            gpio_pin_set_dt(&led, state1 || state2);
         }
         k_msleep(sleep_ms);
     }
